4dd73efbeb00ab8d2bded20ff0386e3d0fc01660
fsrafaela <rafamiller96@gmail.com>
2026-02-17 20:49:35 -0300

    exercicio 2.1 - Criando sistema de placar com as variantes const e let

---
+++ exercicio_2.1.js
@@ -0,0 +1,10 @@
+const nomeJogador = "Ana";
+
+let pontuacao = 0;
+
+console.log("Jogador:", nomeJogador);
+console.log("Pontuação inicial:", pontuacao);
+
+pontuacao = 10;
+
+console.log("Pontuação atualizada:", pontuacao);
