3bac5737bbc8e5fe51c448138c101b6c4e3c3a7e
fsrafaela <rafamiller96@gmail.com>
2026-02-17 20:55:21 -0300

    Exercicio 2.2 - Criando variaveis de diferentes tipos e exibido com typeof

---
+++ exercicio_2.2.js
@@ -0,0 +1,9 @@
+let nome = "Rafaela";
+let idade = 29;
+let gostaDePizza = true;
+let cidade;
+
+console.log("Nome:", nome, "| Tipo:", typeof nome);
+console.log("Idade:", idade, "| Tipo:", typeof idade);
+console.log("Gosta de Pizza:", gostaDePizza, "| Tipo:", typeof gostaDePizza);
+console.log("Cidade:", cidade, "| Tipo:", typeof cidade);
