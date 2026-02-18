44348ffc4ba2b89b9d78d0b29f779256c9400406
fsrafaela <rafamiller96@gmail.com>
2026-02-17 21:58:17 -0300

    Exercicio 6.1 - Usando for para exibir numeros pares e listar os personagens de THE LAST OF US com indice

---
+++ exercicio_6.1.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+let theLastOfUs = ["Joel", "Ellie", "Abby", "Tommy", "Dina", "Tess", "Bill", "Henry", "Sam", "Marlene"];
+
+for (let i = 0; i < theLastOfUs.length; i++) {
+    console.log(i + ": " + theLastOfUs[i]);
+}
