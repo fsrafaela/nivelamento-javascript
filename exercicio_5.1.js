a687076fd1465320a07594b6c4659cab9daf6401
fsrafaela <rafamiller96@gmail.com>
2026-02-17 21:40:23 -0300

    Exercicio 5.1 -  Criando verificador de notas com if else

---
+++ exercicio_5.1.js
@@ -0,0 +1,20 @@
+let nota = 10;
+
+if (nota >= 7) {
+    console.log("Nota:", nota, "- Aprovado");
+} else if (nota >= 5 && nota < 7) {
+    console.log("Nota:", nota, "- Recuperação");
+} else {
+    console.log("Nota:", nota, "- Reprovado");
+}
+
+nota = 6;
+
+
+if (nota >= 7) {
+    console.log("Nota:", nota, "- Aprovado");
+} else if (nota >= 5 && nota < 7) {
+    console.log("Nota:", nota, "- Recuperação");
+} else {
+    console.log("Nota:", nota, "- Reprovado");
+}
