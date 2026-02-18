d27429be17cc578421e0975ebc9baa777de6fd4f
fsrafaela <rafamiller96@gmail.com>
2026-02-17 21:04:29 -0300

    Exercicio 3.1 - Simulando sistema de delivery com null e undefined

---
+++ exercicio_3.1.js
@@ -0,0 +1,11 @@
+let enderecoEntrega = null;
+let numeroPedido;
+
+console.log("Endereço de entrega:", enderecoEntrega);
+console.log("Número do pedido:", numeroPedido);
+
+enderecoEntrega = "Rua das Flores, 1417";
+numeroPedido = 1266;
+
+console.log("Endereço de entrega atualizado:", enderecoEntrega);
+console.log("Número do pedido atualizado:", numeroPedido);
