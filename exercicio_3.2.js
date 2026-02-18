8e350ff38e341bffc54bb753d538382bda3beafb
fsrafaela <rafamiller96@gmail.com>
2026-02-17 21:17:11 -0300

    Exercicio 3.2 - Criando objeto aluno e adicionada propriedade nota

---
+++ exercicio_3.2.js
@@ -0,0 +1,13 @@
+let aluno = {
+    nome: "Rafaela",
+    matricula: "561671"
+};
+
+console.log("Nome:", aluno.nome);
+console.log("Matrícula:", aluno.matricula);
+
+console.log("Nota:", aluno.nota);
+
+aluno.nota = 9.5;
+
+console.log("Nota atualizada:", aluno.nota);
