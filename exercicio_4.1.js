096e15b45a4d1359ef07259731bf994416603c24
fsrafaela <rafamiller96@gmail.com>
2026-02-17 21:30:52 -0300

    exercicio 4.1 - criado array tarefas e adicionado nova tarefa com push

---
+++ exercicio_4.1.js
@@ -0,0 +1,10 @@
+let tarefas = ["Estudar DevOps", "Fazer exercícios de JavaScript", "Ir à academia"];
+
+console.log("Primeira tarefa:", tarefas[0]);
+console.log("Última tarefa:", tarefas[tarefas.length - 1]);
+
+console.log("Total de tarefas:", tarefas.length);
+
+tarefas.push("Ler um livro");
+
+console.log("Novo total de tarefas:", tarefas.length);
